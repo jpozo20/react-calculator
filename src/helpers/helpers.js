1af06c68d331365d8abbe244fe0b67c01f633a93
Jefry Pozo <jefrypozo@gmail.com>
2020-01-13 19:11:37 -0400

    Add basic keyboard handling

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,8 +1,58 @@
+import Constants from "./Constants";
+
 const Helpers = {
 	decodeHtml: (htmlEntity) => {
 		var txt = document.createElement("textarea");
 		txt.innerHTML = htmlEntity;
 		return txt.value;
+	},
+	getOperationFromKeyPress: (event) => {
+		const eventKey = event.key;
+		const result = {
+			operation: "",
+			action: "",
+		};
+
+		switch (eventKey) {
+			case Constants.OperatorKeys.Add:
+				result.operation = Constants.OperatorKeys.Add;
+				result.action = "operation";
+				break;
+			case Constants.OperatorKeys.Substract:
+				result.operation = Constants.OperatorKeys.Substract;
+				result.action = "operation";
+				break;
+			case Constants.OperatorKeys.Equals:
+				result.operation = Constants.OperatorKeys.Equals;
+				result.action = "operation";
+				break;
+			case "/":
+				result.operation = Constants.OperatorKeys.Division;
+				result.action = "operation";
+				break;
+			case "*":
+				result.operation = Constants.OperatorKeys.Multiply;
+				result.action = "operation";
+				break;
+			case "Backspace":
+			case "Delete":
+				result.operation = Constants.OperatorKeys.Delete;
+				result.action = "operation";
+				break;
+			case "Escape":
+				result.operation = Constants.FunctionKeys.Clear;
+				result.action = "function";
+				break;
+			case Constants.FunctionKeys.Percentage:
+				result.operation = Constants.FunctionKeys.Percentage;
+				result.action = "function";
+				break;
+
+			default:
+				break;
+		}
+
+		return result;
 	},
 };
 
