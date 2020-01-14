6332a2c16d1a0fe753201bb795ed98fd46a3c1e6
Jefry Pozo <jefrypozo@gmail.com>
2020-01-13 20:18:59 -0400

    Updates

---
+++ precache-manifest.d3dd11cc4710454d2fa7e5f7fd38dfe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d445664f5040d77fae71778c5fd5e9",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "80af1577ebdc320c65ba",
+    "url": "/react-calculator/static/css/main.bc974092.chunk.css"
+  },
+  {
+    "revision": "0ec8c7ea57fc59461103",
+    "url": "/react-calculator/static/js/2.c01c01c7.chunk.js"
+  },
+  {
+    "revision": "80af1577ebdc320c65ba",
+    "url": "/react-calculator/static/js/main.c392a2fa.chunk.js"
+  },
+  {
+    "revision": "9538c7b0b7ce9317097d",
+    "url": "/react-calculator/static/js/runtime-main.5733824d.js"
+  }
+]);
